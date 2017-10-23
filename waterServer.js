70f1a28103ca96ba9996096a328818d864001539
Steven Foreman <stevenf@kainos.com>
2017-10-23 14:54:07 +0100

    Changed soooo much stuff during sprint planning

--- waterServer.js
+++ waterServer.js
@@ -1,16 +1,23 @@
 /*Ok this is gonna be a node server about water*/
 
-var http = require('http');
+var express = require('express');
+var app = express();
 var fs = require('fs');
+app.use(express.static(__dirname + '/public/'));
 
-http.createServer(function(req,res) {
+
+
+/*http.createServer(function(req,res) {
+	var filePath = path.join(__dirname,'index.html');
+	
+
 	fs.readFile('index.html',function(err,data) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.write(data);
 	res.write("This is a water website");
 	res.end();
+}); */
+app.get('waterOpinions.txt',function (req,res){
+	res.sendFile("./waterOpinions.txt");
 });
-http.get('waterOpinions.txt',function (response){
-	reponse.send("./waterOpinions.txt");
-})
-}).listen(8089);
+app.listen(8089);
